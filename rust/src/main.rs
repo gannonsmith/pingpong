83265b3332c466d9f982098cc4c7c819e2b9e563
Gannon Smith <gansmith@umich.edu>
2022-06-16 19:57:28 -0400

    add fn handle_connection

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    //Read arguments
+    //let args: Vec<String> = env::args().collect();
+    //println!("{:?}", args);
+
+    //let protocol = &args[1];
+    //let address = &args[2];
+
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
