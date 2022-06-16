e103bce4873ee4b6e2c0fc227470a07f03ad5c19
Gannon Smith <gansmith@umich.edu>
2022-06-16 19:36:18 -0400

    removing client.rs and server.rs

--- rust/src/bin/server.rs
+++
@@ -1,9 +0,0 @@
-use std::io::{self, Bufreader, BufWriter};
-use std::net::{SocketAddr, TcpListener, TcpStream};
-
-use structopt::StructOpt;
-
-fn main() -> io::Result<()> {
-    let args = Args::from_args();
-
-}
