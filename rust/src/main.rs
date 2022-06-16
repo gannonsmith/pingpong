285d4be2942ccf3a25d4810384693c4b94b5b209
Gannon Smith <gansmith@umich.edu>
2022-06-16 19:22:18 -0400

    developing main

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,29 @@
+mod client;
+
+use std::env;
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 || (args[1] != "tcp" && args[1] != "udp") || args[2].len() != 14{
+        println!("Enter files in format: cargo run [protocol] [ip]");
+        return;
+    }
+
+    let protocol = &args[1];
+    let ip = &args[2];
+    println!("Using protocol {} on {}", protocol, ip);
+    connect(ip);
+
+
 }
+
+fn connect(ip: &String) {
+    let mut stream = TcpStream::connect(ip).expect("Connection failed...")?;
+    let mut buf :[u8];
+    stream.read(&mut buf);
+
+}
+
+
