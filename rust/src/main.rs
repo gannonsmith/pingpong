db949ceb4f0f5da790e4e136cc0ddfd35624b97b
Gannon Smith <gansmith@umich.edu>
2022-06-17 02:10:33 -0400

    successful server/client connection

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,6 @@
-use std::env;
+use std::{env, thread};
 use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream};
+use std::net::{Shutdown, TcpListener, TcpStream};
 
 fn main() {
     //Read arguments
@@ -11,29 +11,44 @@ fn main() {
     //let address = &args[2];
 
     let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+    println!("Server listening on port 8000");
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
-
-        println!("Connection Success!");
-        //handle_connection(stream);
+        println!("i");
+        match stream {
+            Ok(stream) => {
+                println!("New connection: {}", stream.peer_addr().unwrap());
+                thread::spawn(move|| {
+                    handle_connection(stream);
+                });
+            }
+            Err(e) => {
+                println!("Error: {}", e);
+            }
+        }
     }
+    drop(listener);
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
-    stream.read(&mut buffer).unwrap();
-
-    if String::from_utf8_lossy(&buffer[..]) == "ping" {
-        //write pong
-    }
-    if String::from_utf8_lossy(&buffer[..]) == "pong" {
-        //write ping
-    }
-
-    //let response = "HTTP/1.1 200 OK\r\n\r\n";
-
-    //stream.write(response.as_bytes()).unwrap();
-   // stream.flush().unwrap();
+   while match stream.read(&mut buffer) {
+       Ok(size) => {
+           println!("{}::{}", String::from_utf8_lossy(&buffer[..]).replace("\n", ""), String::from("ping"));
+           if String::from_utf8_lossy(&buffer[..]).replace("\n", "") == String::from("ping") {
+               stream.write("pong".as_bytes()).unwrap();
+           } else if String::from_utf8_lossy(&buffer[..]).replace("\n", "") == String::from("pong") {
+               stream.write("ping".as_bytes()).unwrap();
+           } else {
+               stream.write("error".as_bytes()).unwrap();
+           }
+           true
+       },
+       Err(_) => {
+           println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+           stream.shutdown(Shutdown::Both).unwrap();
+           false
+       }
+   } {}
 }
